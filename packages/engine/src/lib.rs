45ff3f4bccd2e1d4cc4a42cfdc70935c3cc39424
Allan Kimmer Jensen <hi@akj.io>
2026-08-04 17:13:43 +0200

    feat(engine): flask effects, unique flasks, charge bonuses (power/frenzy/endurance)

--- packages/engine/src/lib.rs
+++ packages/engine/src/lib.rs
@@ -19,6 +19,8 @@ pub mod keystones;
 pub mod ascendancy;
 pub mod weapons;
 pub mod minions;
+pub mod flasks;
+pub use flasks::{get_flask_mods, charge_mods};
 pub use weapons::{calc_weapon_dps, find_weapon_base, WeaponDps};
 pub use stat_parser::{parse_stat_line, parse_stats};
 pub use supports::get_support_modifiers;
