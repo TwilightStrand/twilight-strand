92887fe4a3927588f1eab205aa5fbd9e221d6272
Allan Kimmer Jensen <hi@akj.io>
2026-08-04 14:54:00 +0200

    feat(engine): wire Rust WASM into browser, init alongside Lua

---
+++ apps/web/engine/cluster-types.ts
@@ -0,0 +1,30 @@
+export interface ClusterJewelBase {
+  name: string;
+  type: "large" | "medium" | "small";
+  enchant: string;
+  passiveCount: number;
+  notables: string[];
+  smallPassiveType: string;
+}
+
+export interface ClusterNotable {
+  name: string;
+  stats: string[];
+  weight: number;
+}
+
+export interface ClusterSearchResult {
+  jewel: ClusterJewelBase;
+  notables: ClusterNotable[];
+  estimatedPrice: number;
+  dpsGain: number;
+  ehpGain: number;
+  valueScore: number;
+}
+
+export interface ClusterSearchParams {
+  type: "large" | "medium" | "small" | "any";
+  maxPrice: number;
+  sortBy: "dps" | "ehp" | "value" | "price";
+  minDpsGain: number;
+}
